623681d07f9361962b387992164a080edca0e5ab
d.tetsuya.takano <takano_tetsuya@hand.co.jp>
2018-11-28 20:23:24 +0900

    アセットロードまわりのサンプル

--- program/Chipstar/Assets/Chipstar/Example/Runtime/AssetLoadExample.cs
+++ program/Chipstar/Assets/Chipstar/Example/Runtime/AssetLoadExample.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using System;
 using System.IO;
+using Chipstar.AssetLoad;
+
 namespace Chipstar.Example
 {
     public class AssetLoadExample : MonoBehaviour
@@ -20,7 +22,9 @@ namespace Chipstar.Example
 		//========================================
 		//	変数
 		//========================================
-		private IAssetLoadProvider m_provider = null;
+		private IDisposable					m_disposable = null;
+		private IAssetBundleLoadProvider	m_downloadProvider	 = null;
+		private IAssetLoadProvider          m_assetLoadProvider  = null;
 
 		//========================================
 		//	関数
@@ -29,17 +33,24 @@ namespace Chipstar.Example
 		// Use this for initialization
 		IEnumerator Start()
         {
-			//  https://www.google.co.jp/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png
-			var acccesPoint   = new EntryPoint( Path.Combine( Application.dataPath, "../../build/windows/lz4/" ) );
+			//	接続先
+			var acccesPoint   = new EntryPoint( Path.Combine( Application.dataPath, "../../build/windows/lzma/" ) );
+			//	保存先
 			var cacheStorage  = new EntryPoint( Path.Combine( Application.dataPath, "../../cacheStorage/" ) );
-
+			
+			//	コンテンツカタログ
 			var loadDatabase = new LoadDatabase<BuildMapDataTable, BundleBuildData, AssetBuildData, RuntimeBundlleData>( acccesPoint,"buildMap.json" );
+			//	ローカル保存データベース
 			var cacheDatabase= new CacheDatabase( cacheStorage, "localVersion.json"  );
 
-			var creator      = new SampleJobCreator<RuntimeBundlleData>();
+			m_disposable = loadDatabase;
+
+			//	リクエスト作成
+			var creator      = new SampleJobCreator();
             var jobEngine    = new JobEngine();
 
-            m_provider = new AssetLoadProvider<RuntimeBundlleData>
+			//	DL機能を統合
+            m_downloadProvider = new AssetBundleLoadProvider<RuntimeBundlleData>
                 (
                     loadDatabase	: loadDatabase,
 					cacheDatabase	: cacheDatabase,
@@ -47,26 +58,32 @@ namespace Chipstar.Example
                     dlEngine		: jobEngine
                 );
 
+			//	読み込みリクエスト側機能
+			var factoryContainer = new FactoryContainer
+			(
+				new AssetBundleLoadFactory<RuntimeBundlleData>( loadDatabase ),
+				new ResourcesLoadFactory()
+			);
+			//	統合
+			m_assetLoadProvider = new AssetLoadProvider( factoryContainer );
+
             yield return null;
 
 			//	初期化開始
-            yield return m_provider.InitLoad( );
+            yield return m_downloadProvider.InitLoad( );
 
 			m_cacheListText	.text = cacheDatabase.ToString();
 			m_dlListText	.text = loadDatabase .ToString();
 
-
-			//	完了
+			//	アセットバンドルDL
 			var path = "Assets/BundleTarget/Container 1.prefab";
-			var downloadTask = m_provider.Load( path );
+			var downloadTask = m_downloadProvider.Load( path );
             yield return new WaitWhile(() => !downloadTask.IsCompleted );
 			yield return null;
-			var assetData	= loadDatabase.Find( path );
-			var bundle      = assetData.BundleData;
-			var operation   = bundle.LoadAsync( path );
-			yield return new WaitWhile( () => !operation.isDone );
-
-			var prefab		= operation.asset as GameObject;
+			//	リソースロード
+			var operation = m_assetLoadProvider.LoadAsset<GameObject>( path );
+			yield return operation;
+			var prefab		= operation.Content;
 			var container	= prefab.GetComponent<Container>();
 			var parent		= m_image.transform.parent;
 			foreach( var item in container.List )
@@ -80,12 +97,13 @@ namespace Chipstar.Example
 		}
         private void OnDestroy()
         {
+			m_disposable.Dispose();
         }
 
         private void Update()
         {
-            if( m_provider == null ) { return; }
-            m_provider.DoUpdate();
+            if( m_downloadProvider == null ) { return; }
+            m_downloadProvider.DoUpdate();
         }
     }
 }
