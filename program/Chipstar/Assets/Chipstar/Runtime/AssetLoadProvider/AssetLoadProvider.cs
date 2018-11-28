78291a5884b2d214f7d5fb566e5d80a6e67dc09f
d.tetsuya.takano <takano_tetsuya@hand.co.jp>
2018-11-28 18:49:09 +0900

    アセットロード基礎とResources.Load用機能

---
+++ program/Chipstar/Assets/Chipstar/Runtime/AssetLoadProvider/AssetLoadProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Chipstar.AssetLoad
+{
+	/// <summary>
+	/// アセット読み込みまわりの管理
+	/// </summary>
+	public interface IAssetLoadProvider
+	{
+		ILoadOperation<T>	LoadAsset<T>	( string path ) where T : UnityEngine.Object;
+		AsyncOperation		LoadScene		( string path );
+	}
+
+	/// <summary>
+	/// アセット読み込み統括
+	/// </summary>
+	public class AssetLoadProvider : IAssetLoadProvider
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public ILoadOperation<T> LoadAsset<T>( string path ) where T : UnityEngine.Object
+		{
+			return null;
+		}
+
+		public AsyncOperation LoadScene( string path )
+		{
+			return SceneManager.LoadSceneAsync( path );
+		}
+	}
+}
---
+++ program/Chipstar/Assets/Chipstar/Runtime/AssetLoadProvider/Loader/Operation/LoadOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Chipstar.AssetLoad
+{
+	public interface ILoadOperation<T> where T : UnityEngine.Object
+	{
+		T Content { get; }
+	}
+	/// <summary>
+	/// アセットロード用のタスク
+	/// とりあえずUnityのコルーチンで通すためのモノ
+	/// </summary>
+	public abstract class LoadOperation<T> :
+		CustomYieldInstruction,
+		ILoadOperation<T> where T : UnityEngine.Object
+	{
+		public abstract T Content { get; }
+	}
+}
