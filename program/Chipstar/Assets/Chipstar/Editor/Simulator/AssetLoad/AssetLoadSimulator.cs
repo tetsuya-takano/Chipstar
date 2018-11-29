489f2dedb197969742cc1d18cde937a3db944ff8
d.tetsuya.takano <takano_tetsuya@hand.co.jp>
2018-11-29 21:19:38 +0900

    エディタ用のろーどまわり

---
+++ program/Chipstar/Assets/Chipstar/Editor/Simulator/AssetLoad/AssetLoadSimulator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chipstar.Downloads
+{
+	/// <summary>
+	/// エディタ用のアセットロード機能
+	/// </summary>
+	public sealed class AssetLoadSimulator : IAssetLoadProvider
+	{
+		//=================================
+		//	プロパティ
+		//=================================
+		private IFactoryContainer Container { get; set; }
+
+		//=================================
+		//	関数
+		//=================================
+
+		public AssetLoadSimulator()
+		{
+			Container = new FactoryContainer
+				(
+					new ResourcesLoadFactory()
+				);
+		}
+
+		/// <summary>
+		/// 破棄処理
+		/// </summary>
+		public void Dispose()
+		{
+
+		}
+		/// <summary>
+		/// アセット
+		/// </summary>
+		public IAssetLoadOperation<T> LoadAsset<T>( string path ) where T : UnityEngine.Object
+		{
+			return null;
+		}
+
+		/// <summary>
+		/// シーン
+		/// </summary>
+		public ISceneLoadOperation LoadLevel( string path )
+		{
+			return null;
+		}
+	}
+}
---
+++ program/Chipstar/Assets/Chipstar/Editor/Simulator/AssetLoad/Factory/EditorLoadAssetOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chipstar.Downloads
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public sealed class EditorLoadAssetOperation<T> 
+		: AssetLoadOperation<T>
+		where T : UnityEngine.Object
+	{
+		//===============================
+		//	プロパティ
+		//===============================
+		public override T		Content		{ get { return null; } }
+		public override bool	keepWaiting { get { return false; } }
+
+		//===============================
+		//	関数
+		//===============================
+
+		/// <summary>
+		/// 破棄
+		/// </summary>
+		protected override void DoDispose()
+		{
+		}
+	}
+}
