327349e89cb5d0d5b3232fb53c104dd2cba02ad1
d.tetsuya.takano <takano_tetsuya@hand.co.jp>
2018-11-29 14:31:13 +0900

    Resourcesロードの判定を修正

--- program/Chipstar/Assets/Chipstar/Runtime/AssetLoadProvider/OperateFactory/ResourcesLoadFactory.cs
+++ program/Chipstar/Assets/Chipstar/Runtime/AssetLoadProvider/OperateFactory/ResourcesLoadFactory.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using UnityEngine;
 namespace Chipstar.AssetLoad
 {
@@ -10,13 +11,27 @@ namespace Chipstar.AssetLoad
 	/// </summary>
 	public sealed class ResourcesLoadFactory : IAssetLoadFactory
 	{
+		//==============================
+		//	const
+		//==============================
+		private const           string  PATTERN = "(/|\\s)Resources/(.*)";
+		private static readonly Regex   m_regex = new Regex( PATTERN );
+
+		//==============================
+		//	変数
+		//==============================
+
+		//==============================
+		//	関数
+		//==============================
+
 		/// <summary>
 		/// 取得可能かどうか
 		/// </summary>
 		public bool CanLoad( string path )
 		{
 			//	拡張子指定ナシはたぶんResources
-			return !Path.HasExtension( path ) || path.Contains( "/Resources/" );
+			return !Path.HasExtension( path ) || m_regex.IsMatch( path );
 		}
 
 		/// <summary>
@@ -24,7 +39,20 @@ namespace Chipstar.AssetLoad
 		/// </summary>
 		public ILoadOperation<T> Create<T>( string path ) where T : UnityEngine.Object
 		{
-			return new ResourcesLoadOperation<T>( Resources.LoadAsync<T>( Path.GetFileNameWithoutExtension( path ) ) );
+			var match = m_regex.Match( path );
+			if( !match.Success )
+			{
+				return null;
+			}
+
+			//	Resources以下を拾う
+			var accessKey = match.Groups[2].Value;
+			if( Path.HasExtension( accessKey ))
+			{
+				//	拡張子は削る
+				accessKey = accessKey.Replace( Path.GetExtension( accessKey ), string.Empty );
+			}
+			return new ResourcesLoadOperation<T>( Resources.LoadAsync<T>( accessKey ) );
 		}
 
 		/// <summary>
