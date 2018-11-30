9f07a18d0b23febd19292d85bc1b0ba5e142f2de
d.tetsuya.takano <takano_tetsuya@hand.co.jp>
2018-11-30 12:58:57 +0900

    統括クラスを後付で作成するため

--- program/Chipstar/Assets/Chipstar/Runtime/AssetManager/AssetManager.cs
+++ program/Chipstar/Assets/Chipstar/Runtime/AssetManager/AssetManager.cs
@@ -1,4 +1,5 @@
 ﻿using Chipstar.Downloads;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,40 +7,41 @@ using UnityEngine;
 namespace Chipstar
 {
 	/// <summary>
-	/// リソース管理機能の統合クラス
+	/// リソース管理機能の統合インターフェイス
 	/// </summary>
-	public interface IAssetManager
-	{
-
-	}
-	/// <summary>
-	/// アセット管理クラス
-	/// </summary>
-	public abstract class AssetManager : IAssetManager
+	public interface IAssetManager : IDisposable
 	{
 		/// <summary>
 		/// 初期化
 		/// </summary>
-		public abstract IEnumerator	Setup();
+		IEnumerator	Setup();
 
 		/// <summary>
 		/// 事前ロード
 		/// </summary>
-		public abstract IEnumerator Preload( string path );
+		IEnumerator Preload( string path );
 
 		/// <summary>
 		/// アセットの読み込み
 		/// </summary>
-		public abstract IAssetLoadOperation<T>	LoadAsset<T>() where T : UnityEngine.Object;
+		IAssetLoadOperation<T>	LoadAsset<T>( string assetPath ) where T : UnityEngine.Object;
 
 		/// <summary>
 		/// シーン遷移
 		/// </summary>
-		public abstract ISceneLoadOperation LoadLevel( string scenePath );
+		ISceneLoadOperation LoadLevel( string scenePath );
 
 		/// <summary>
 		/// シーン加算
 		/// </summary>
-		public abstract ISceneLoadOperation LoadLevelAdditive( string scenePath );
+		ISceneLoadOperation LoadLevelAdditive( string scenePath );
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public static partial class AssetManager
+	{
+
 	}
 }
