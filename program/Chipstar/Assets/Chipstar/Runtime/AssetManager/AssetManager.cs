b4030fb39d0aeace260213c8a37a84ddebfd35ba
d.tetsuya.takano <takano_tetsuya@hand.co.jp>
2018-11-30 11:52:22 +0900

    アセット管理まわりの統括クラス

---
+++ program/Chipstar/Assets/Chipstar/Runtime/AssetManager/AssetManager.cs
@@ -0,0 +1,45 @@
+﻿using Chipstar.Downloads;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chipstar
+{
+	/// <summary>
+	/// リソース管理機能の統合クラス
+	/// </summary>
+	public interface IAssetManager
+	{
+
+	}
+	/// <summary>
+	/// アセット管理クラス
+	/// </summary>
+	public abstract class AssetManager : IAssetManager
+	{
+		/// <summary>
+		/// 初期化
+		/// </summary>
+		public abstract IEnumerator	Setup();
+
+		/// <summary>
+		/// 事前ロード
+		/// </summary>
+		public abstract IEnumerator Preload( string path );
+
+		/// <summary>
+		/// アセットの読み込み
+		/// </summary>
+		public abstract IAssetLoadOperation<T>	LoadAsset<T>() where T : UnityEngine.Object;
+
+		/// <summary>
+		/// シーン遷移
+		/// </summary>
+		public abstract ISceneLoadOperation LoadLevel( string scenePath );
+
+		/// <summary>
+		/// シーン加算
+		/// </summary>
+		public abstract ISceneLoadOperation LoadLevelAdditive( string scenePath );
+	}
+}
