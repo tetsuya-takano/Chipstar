9f902979a38e0b7042dded41d150e5852f3cbd0e
d.tetsuya.takano <takano_tetsuya@hand.co.jp>
2018-11-30 20:48:22 +0900

    Unloader作るんですよ

---
+++ program/Chipstar/Assets/Chipstar/Runtime/AssetUnloadProvider/AssetUnloadProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chipstar.Downloads
+{
+	/// <summary>
+	/// 破棄管理
+	/// </summary>
+	public interface IAssetUnloadProvider : IDisposable
+	{
+
+	}
+	/// <summary>
+	/// 
+	/// </summary>
+	public class AssetUnloadProvider : IAssetUnloadProvider
+	{
+		/// <summary>
+		/// 破棄
+		/// </summary>
+		public void Dispose()
+		{
+		}
+	}
+}
