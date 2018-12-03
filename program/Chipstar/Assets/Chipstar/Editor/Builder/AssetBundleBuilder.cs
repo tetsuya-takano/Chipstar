26556c6a17d9e3a0bcb6800a29724aabd2e67590
d.tetsuya.takano <takano_tetsuya@hand.co.jp>
2018-12-03 15:10:04 +0900

    ビルドまわりで注入するタイミングの移動

--- program/Chipstar/Assets/Chipstar/Editor/Builder/AssetBundleBuilder.cs
+++ program/Chipstar/Assets/Chipstar/Editor/Builder/AssetBundleBuilder.cs
@@ -6,44 +6,94 @@ using UnityEditor;
 
 namespace Chipstar.Builder
 {
-    /// <summary>
-    /// ビルド用クラス
-    /// </summary>
-	public class AssetBundleBuilder<TPackageData,TBuildData,TBuildResult> 
-        where TPackageData  : IABPackageData<TBuildData>
+	public interface IAssetBundleBuilder<TBuildResult> : IDisposable
+		where TBuildResult : IABBuildResult
+	{
+		TBuildResult Build();
+	}
+	/// <summary>
+	/// ビルド用クラス
+	/// </summary>
+	public class AssetBundleBuilder<TPackageData,TBuildData,TBuildResult>
+		: IAssetBundleBuilder<TBuildResult>
+		where TPackageData  : IABPackageData<TBuildData>
         where TBuildData    : IABBuildData
         where TBuildResult  : IABBuildResult
     {
+		//============================================
+		//	プロパティ
+		//============================================
+		private IABBuildConfig								Config			{ get; set; }
+		private IABPackageSettings<TPackageData,TBuildData> PackageSettings	{ get; set; }
+
+		private IABBuildFileFilter							FileFilter		{ get; set; }
+
+		private IABBuildPreProcess<TBuildData>				PreProcess	{ get; set; }
+		private IABBuildProcess<TBuildData,TBuildResult>	BuildProcess{ get; set; }
+		private IABBuildPostProcess<TBuildData,TBuildResult>PostProcess { get; set; }
+		//============================================
+		//	関数
+		//============================================
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public AssetBundleBuilder
+		(
+			IABBuildConfig									config,
+			IABBuildFileFilter								fileFilter,
+			IABPackageSettings<TPackageData, TBuildData>	packageSettings,
+			IABBuildProcess<TBuildData, TBuildResult>		buildProcess,
+			IABBuildPreProcess<TBuildData>					preProcess,
+			IABBuildPostProcess<TBuildData, TBuildResult>	postProcess
+			)
+		{
+			Config			= config;
+			PackageSettings = packageSettings;
+			FileFilter      = fileFilter;
+			PreProcess		= preProcess;
+			BuildProcess    = buildProcess;
+			PostProcess     = postProcess;
+		}
+
+		/// <summary>
+		/// 破棄処理
+		/// </summary>
+		public void Dispose()
+		{
+			Config			= null;
+			PackageSettings = null;
+			FileFilter      = null;
+			PreProcess      = null;
+			BuildProcess    = null;
+			PostProcess     = null;
+		}
+
         /// <summary>
         /// ビルド
         /// </summary>
-        public virtual void Build( 
-            IABBuildConfig                                  config       ,
-            IABBuildFileFilter                              fileFilter   ,
-            IABPackageSettings   <TPackageData,TBuildData>  packageSettings,
-            IABBuildProcess      <TBuildData,TBuildResult>  buildProcess ,
-            IABBuildPreProcess   <TBuildData>               preProcess   ,
-            IABBuildPostProcess  <TBuildData,TBuildResult>  postProcess  
-        )
+        public virtual TBuildResult Build( )
         {
             //  ビルド対象アセットの絞り込み
-            var buildAssets     = FilteringBuildAssets( AssetDatabase.GetAllAssetPaths(), fileFilter );
+            var buildAssets     = FilteringBuildAssets( AssetDatabase.GetAllAssetPaths(), FileFilter );
 
-            var packageGroup    = CreateBuildPackageGroup( packageSettings );
+            var packageGroup    = CreateBuildPackageGroup( PackageSettings );
 
 			//	ビルドマップの生成
-			var assetBundleList = CreateAssetBundlePackage( config, buildAssets, packageGroup );
+			var assetBundleList = CreateAssetBundlePackage( Config, buildAssets, packageGroup );
 
             //  事前処理
-            preProcess.OnProcess( assetBundleList );
+            PreProcess.OnProcess( assetBundleList );
 			
 			//	ビルド実行
-			var result = buildProcess.Build( config, assetBundleList );
+			var result = BuildProcess.Build( Config, assetBundleList );
 
 			//	事後処理
-            postProcess.OnProcess( config, result, assetBundleList );
+            PostProcess.OnProcess( Config, result, assetBundleList );
 
             AssetDatabase.Refresh();
+
+			return result;
 		}
 
         protected virtual IList<TPackageData> CreateBuildPackageGroup( IABPackageSettings<TPackageData, TBuildData> packageSettings )
@@ -88,4 +138,11 @@ namespace Chipstar.Builder
             return pack.Build( packagedAssets );
         }
     }
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public static partial class AssetBundleBuilder
+	{
+	}
 }
