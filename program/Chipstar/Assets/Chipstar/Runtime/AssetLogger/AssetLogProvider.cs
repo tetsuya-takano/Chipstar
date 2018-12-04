168a2ca949723becb0d3c1799bc111317a607861
d.tetsuya.takano <takano_tetsuya@hand.co.jp>
2018-12-04 12:55:26 +0900

    Loggerの実装見直し

---
+++ program/Chipstar/Assets/Chipstar/Runtime/AssetLogger/AssetLogProvider.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chipstar.Downloads
+{
+	public interface IAssetLogProvider : IDisposable
+	{
+		void Log	<TLogger>( string log ) where TLogger : IAssetLogger;
+		void Warning<TLogger>( string log ) where TLogger : IAssetLogger;
+		void Error	<TLogger>( string log ) where TLogger : IAssetLogger;
+	}
+	/// <summary>
+	/// ログを出さない
+	/// </summary>
+	public sealed class NothingLogger : IAssetLogProvider
+	{
+		public void Dispose() { }
+		public void Error<TLogger>	( string log ) where TLogger : IAssetLogger { }
+		public void Log<TLogger>	( string log ) where TLogger : IAssetLogger { }
+		public void Warning<TLogger>( string log ) where TLogger : IAssetLogger { }
+	}
+	/// <summary>
+	/// ロガー
+	/// </summary>
+	public class AssetLogProvider
+		: IAssetLogProvider
+	{
+		//===================================
+		//	class
+		//===================================
+
+		//===================================
+		//	変数
+		//===================================
+		private IAssetLogger[] m_loggers = null;
+
+		//===================================
+		//	プロパティ
+		//===================================
+		public int LogLevel { get; set; }
+
+		//===================================
+		//	関数
+		//===================================
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public AssetLogProvider( params IAssetLogger[] args )
+		{
+			m_loggers = args;
+		}
+
+		/// <summary>
+		/// 破棄処理
+		/// </summary>
+		public void Dispose()
+		{
+
+		}
+		/// <summary>
+		/// ログ表示
+		/// </summary>
+		public void Log<TLogger>( string log ) where TLogger : IAssetLogger
+		{
+			foreach( var logger in m_loggers )
+			{
+				//	あるかどうか
+				if( !( logger is TLogger	 )) { continue; }
+				if( !logger.CanLog( LogLevel )) { continue; }
+				logger.Log( log );
+			}
+		}
+		/// <summary>
+		/// 警告
+		/// </summary>
+		public void Warning<TLogger>( string log ) where TLogger : IAssetLogger
+		{
+			foreach( var logger in m_loggers )
+			{
+				//	あるかどうか
+				if( !( logger is TLogger ) ) { continue; }
+				if( !logger.CanLog( LogLevel ) ) { continue; }
+				logger.Warning( log );
+			}
+		}
+		/// <summary>
+		/// エラー
+		/// </summary>
+		public void Error<TLogger>( string log ) where TLogger : IAssetLogger
+		{
+			foreach( var logger in m_loggers )
+			{
+				//	あるかどうか
+				if( !( logger is TLogger ) ) { continue; }
+				if( !logger.CanLog( LogLevel ) ) { continue; }
+				logger.Error( log );
+			}
+		}
+	}
+}
---
+++ program/Chipstar/Assets/Chipstar/Runtime/AssetLogger/Logger/AssetLogger.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chipstar.Downloads
+{
+	/// <summary>
+	/// ログ機能
+	/// </summary>
+	public interface IAssetLogger : IDisposable
+	{
+		void Log	( string log );
+		void Error	( string log );
+		void Warning( string log );
+		bool CanLog ( int logLevel );
+	}
+	/// <summary>
+	/// アセット読み込みログ
+	/// </summary>
+	public interface ILoadAssetLog : IAssetLogger { }
+
+	public interface ILoadLevelLog		: IAssetLogger { }
+	public interface ILoadAdditiveLog	: IAssetLogger { }
+	/// <summary>
+	/// シーン読み込みログ
+	/// </summary>
+	public interface ILoadSceneLog		: ILoadAdditiveLog, ILoadLevelLog { }
+
+	/// <summary>
+	/// ログベースクラス
+	/// </summary>
+	public abstract class AssetLogger : IAssetLogger
+	{
+		//================================
+		//	プロパティ
+		//================================
+		protected int ViewLevel { get; set; }
+
+		//================================
+		//	関数
+		//================================
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public AssetLogger( int level )
+		{
+			ViewLevel = level;
+		}
+
+		/// <summary>
+		/// 破棄
+		/// </summary>
+		public void Dispose()
+		{
+		}
+
+
+		/// <summary>
+		/// ログを出すかどうか
+		/// </summary>
+		public bool CanLog( int logLevel )
+		{
+			return DoCanlog( logLevel );
+		}
+		/// <summary>
+		/// エラー
+		/// </summary>
+		public void Error( string log )
+		{
+			DoError( log );
+		}
+		/// <summary>
+		/// ログ
+		/// </summary>
+		public void Log( string log )
+		{
+			DoLog( log );
+		}
+
+		/// <summary>
+		/// 警告
+		/// </summary>
+		public void Warning( string log )
+		{
+			DoWarning( log );
+		}
+
+
+		protected virtual bool DoCanlog( int level )
+		{
+			return ViewLevel <= level;
+		}
+		protected virtual void DoLog	( string log ) { }
+		protected virtual void DoWarning( string log ) { }
+		protected virtual void DoError	( string log ) { }
+	}
+}
